a07b425a763520145e62651f9c9dd5659f4c858b
Ben Edwards <hello@benedfit.com>
2026-06-25 11:23:42 +0100

    chore: tidy up and preload fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,25 @@ import { type AppProps } from 'next/app'
 import type { FC } from 'react'
 
 import config from '~config'
+import { default as fonts } from '~styles/font-faces.module.scss'
 import theme from '~theme'
 
+const meta = {
+  additionalLinkTags: Object.values(fonts).map(fileName => {
+    const type = fileName.split('.').at(-1)
+
+    return {
+      rel: 'preload',
+      href: `/fonts/${fileName}`,
+      as: 'font',
+      type: `font/${type}`,
+      crossOrigin: 'anonymous'
+    }
+  })
+}
+
 const App: FC<AppProps> = props => (
-  <AppPage {...props} theme={theme} config={config} />
+  <AppPage {...props} theme={theme} config={config} meta={meta} />
 )
 
 export default App
